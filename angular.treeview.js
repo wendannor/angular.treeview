6997cb4a15b11e8e3c7ccb901885262c2ff8a639
Wendannor <jordan.sarre@gmail.com>
2015-02-26 10:16:42 +0100

    Show/Hide leaf type and function triggering when node selected

--- angular.treeview.js
+++ angular.treeview.js
@@ -1,98 +1,112 @@
 /*
-	@license Angular Treeview version 0.1.6
-	ⓒ 2013 AHN JAE-HA http://github.com/eu81273/angular.treeview
-	License: MIT
+ @license Angular Treeview version 0.1.6
+ ⓒ 2013 AHN JAE-HA http://github.com/eu81273/angular.treeview
+ License: MIT
 
 
-	[TREE attribute]
-	angular-treeview: the treeview directive
-	tree-id : each tree's unique id.
-	tree-model : the tree model on $scope.
-	node-id : each node's id
-	node-label : each node's label
-	node-children: each node's children
+ [TREE attribute]
+ angular-treeview: the treeview directive
+ tree-id : each tree's unique id.
+ tree-model : the tree model on $scope.
+ node-id : each node's id
+ node-label : each node's label
+ node-children: each node's children
+ on-selected-node: function called when a node is selected
+ show-leafs: should the node of type 'file' (without children) be displayed
+ <div
+ angular-treeview="true"
+ tree-id="tree"
+ tree-model="roleList"
+ node-id="roleId"
+ node-label="roleName"
+ node-children="children"
+ on-selected-node="onSelectedNode(node)"
+ show-leafs="true">
+ </div>
+ */
 
-	<div
-		data-angular-treeview="true"
-		data-tree-id="tree"
-		data-tree-model="roleList"
-		data-node-id="roleId"
-		data-node-label="roleName"
-		data-node-children="children" >
-	</div>
-*/
+(function (angular) {
+    'use strict';
 
-(function ( angular ) {
-	'use strict';
+    angular.module('angularTreeview', [])
+        .directive('treeModel', ['$compile', '$parse', function ($compile, $parse) {
+            return {
+                restrict: 'A',
+                link: function (scope, element, attrs) {
+                    //tree id
+                    var treeId = attrs.treeId;
 
-	angular.module( 'angularTreeview', [] ).directive( 'treeModel', ['$compile', function( $compile ) {
-		return {
-			restrict: 'A',
-			link: function ( scope, element, attrs ) {
-				//tree id
-				var treeId = attrs.treeId;
-			
-				//tree model
-				var treeModel = attrs.treeModel;
+                    //tree model
+                    var treeModel = attrs.treeModel;
 
-				//node id
-				var nodeId = attrs.nodeId || 'id';
+                    //node id
+                    var nodeId = attrs.nodeId || 'id';
 
-				//node label
-				var nodeLabel = attrs.nodeLabel || 'label';
+                    //node label
+                    var nodeLabel = attrs.nodeLabel || 'label';
 
-				//children
-				var nodeChildren = attrs.nodeChildren || 'children';
+                    //children
+                    var nodeChildren = attrs.nodeChildren || 'children';
 
-				//tree template
-				var template =
-					'<ul>' +
-						'<li data-ng-repeat="node in ' + treeModel + '"  ng-switch="node.collapsed">' +
-							'<i class="collapsed" data-ng-show="node.' + nodeChildren + '.length && node.collapsed" data-ng-click="' + treeId + '.selectNodeHead(node)"></i>' +
-							'<i class="expanded" data-ng-show="node.' + nodeChildren + '.length && !node.collapsed" data-ng-click="' + treeId + '.selectNodeHead(node)"></i>' +
-							'<i class="normal" data-ng-hide="node.' + nodeChildren + '.length"></i> ' +
-							'<span data-ng-class="node.selected" data-ng-click="' + treeId + '.selectNodeLabel(node)">{{node.' + nodeLabel + '}}</span>' +
-							'<div data-ng-hide="node.collapsed" data-tree-id="' + treeId + '" data-tree-model="node.' + nodeChildren + '" data-node-id=' + nodeId + ' data-node-label=' + nodeLabel + ' data-node-children=' + nodeChildren + '  ng-switch-when="false"></div>' +
-						'</li>' +
-					'</ul>';
+                    // show leafs
+                    var showLeafs = (attrs.showLeafs === 'true');
+
+                    //tree template
+                    var template =
+                        '<ul>' +
+                        '<li data-ng-repeat="node in ' + treeModel + '"  ng-switch="node.collapsed">' +
+                        '<i class="collapsed" data-ng-show="node.' + nodeChildren + '&& node.collapsed" data-ng-click="' + treeId + '.selectNodeHead(node)"></i>' +
+                        '<i class="expanded" data-ng-show="node.' + nodeChildren + ' && !node.collapsed" data-ng-click="' + treeId + '.selectNodeHead(node)"></i>' +
+                        '<i class="normal" data-ng-show="!node.' + nodeChildren + ' && ' + showLeafs + '"></i> ' +
+                        '<span data-ng-class="node.selected" data-ng-click="' + treeId + '.selectNodeLabel(node)" ng-if="' + showLeafs + '  && !node.' + nodeChildren + '">{{node.' + nodeLabel + '}}</span>' +
+                        '<span data-ng-class="node.selected" data-ng-click="' + treeId + '.selectNodeLabel(node)" ng-if="node.' + nodeChildren + '">{{node.' + nodeLabel + '}}</span>' +
+                        '<div data-ng-hide="node.collapsed" data-tree-id="' + treeId + '" data-tree-model="node.' + nodeChildren + '" data-node-id=' + nodeId + ' data-node-label=' + nodeLabel + ' data-node-children=' + nodeChildren + '  ng-switch-when="false"></div>' +
+                        '</li>' +
+                        '</ul>';
 
 
-				//check tree id, tree model
-				if( treeId && treeModel ) {
+                    //check tree id, tree model
+                    if (treeId && treeModel) {
 
-					//root node
-					if( attrs.angularTreeview ) {
-					
-						//create tree object if not exists
-						scope[treeId] = scope[treeId] || {};
+                        //root node
+                        if (attrs.angularTreeview) {
 
-						//if node head clicks,
-						scope[treeId].selectNodeHead = scope[treeId].selectNodeHead || function( selectedNode ){
+                            //create tree object if not exists
+                            scope[treeId] = scope[treeId] || {};
 
-							//Collapse or Expand
-							selectedNode.collapsed = !selectedNode.collapsed;
-						};
+                            //if node head clicks,
+                            scope[treeId].selectNodeHead = scope[treeId].selectNodeHead || function (selectedNode) {
 
-						//if node label clicks,
-						scope[treeId].selectNodeLabel = scope[treeId].selectNodeLabel || function( selectedNode ){
+                                //Collapse or Expand
+                                selectedNode.collapsed = !selectedNode.collapsed;
+                            };
 
-							//remove highlight from previous node
-							if( scope[treeId].currentNode && scope[treeId].currentNode.selected ) {
-								scope[treeId].currentNode.selected = undefined;
-							}
+                            //if node label clicks,
+                            scope[treeId].selectNodeLabel = scope[treeId].selectNodeLabel || function (selectedNode) {
 
-							//set highlight to selected node
-							selectedNode.selected = 'selected';
+                                //remove highlight from previous node
+                                if (scope[treeId].currentNode && scope[treeId].currentNode.selected) {
+                                    scope[treeId].currentNode.selected = undefined;
+                                }
 
-							//set currentNode
-							scope[treeId].currentNode = selectedNode;
-						};
-					}
+                                //set highlight to selected node
+                                selectedNode.selected = 'selected';
 
-					//Rendering template.
-					element.html('').append( $compile( template )( scope ) );
-				}
-			}
-		};
-	}]);
-})( angular );
+                                //set currentNode
+                                scope[treeId].currentNode = selectedNode;
+
+                                if (attrs.onSelectedNode) {
+                                    var fn = $parse(attrs.onSelectedNode);
+                                    fn(scope, {node: selectedNode});
+                                }
+
+                            };
+                        }
+
+                        //Rendering template.
+                        element.html('').append($compile(template)(scope));
+                    }
+                }
+            };
+        }]);
+})(angular);
